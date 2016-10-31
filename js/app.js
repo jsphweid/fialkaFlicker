a60d2daac14001236700a5a55facba69b2b2ffaa
Joseph Weidinger <jsphweid@gmail.com>
2016-10-31 16:05:59 -0500

    removed a lot of things from the global namespace

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ var vid;
 var p5Canvas;
 var w = 640,
     h = 480;
-var displayForm = true; // must be the opposite on initialization
 var downloadLinkForVid = 'https://dl.dropboxusercontent.com/u/67477162/perm/vidsForSites/dance.mp4';
 
 var flick = {
+
+    "displayForm" : true,
+
     // defaults
     "whiteCount" : 1,
     "blackCount" : 1,
@@ -18,90 +20,97 @@ var flick = {
     "vid" : 0,
 
     "drawNow" : 0, // 0 black, 1 white, 2 pic
-    "flicker" : false
+    "flicker" : false,
+
+    toggleFlicker : function() {
+        var ele = document.getElementById('myForm');
+        if (this.displayForm) {
+            ele.style.visibility = 'visible';
+            flick.flicker = true;
+            this.displayForm = !this.displayForm;
+        } else {
+            ele.style.visibility = 'hidden';
+            flick.flicker = false;
+            this.displayForm = !this.displayForm;
+        }
+    },
+
+    updateFlick : function() {
+        var form = document.getElementById('myForm');
+        
+        // set
+        flick.whiteCount = form.wh.value;
+        flick.blackCount = form.bl.value;
+        flick.vidCount = form.vi.value;
+    }
+
 };
 
-function toggleFlicker() {
-    var ele = document.getElementById('myForm');
-    if (displayForm) {
-        ele.style.visibility = 'visible';
-        flick.flicker = true;
-        displayForm = !displayForm;
-    } else {
-        ele.style.visibility = 'hidden';
-        flick.flicker = false;
-        displayForm = !displayForm;
-    }
-}
 
-
-function updateFlick() {
-    var form = document.getElementById('myForm');
-    
-    // set
-    flick.whiteCount = form.wh.value;
-    flick.blackCount = form.bl.value;
-    flick.vidCount = form.vi.value;
-    
-}
 
 
 // ===================  p5js  ===================== //
-function setup() {
 
-    // put in canvas div
-    p5Canvas = createCanvas(w, h);
-    p5Canvas.parent('canvas');
+var sketch = function(p) {
 
-    // initialize default loading text...
-    textSize(32);
-    text("loading video...", 10, 30);
+    p.setup = function () {
 
-    // load video
-    vid = createVideo([downloadLinkForVid]); // get vid
+        // put in canvas div
+        p5Canvas = p.createCanvas(w, h);
+        // p5Canvas.parent('canvas');
 
-    // hide default video behavior
-    vid.hide();
+        // initialize default loading text...
+        p.textSize(32);
+        p.text("loading video...", 10, 30);
 
-    vid.loop();
-}
+        // load video
+        vid = p.createVideo([downloadLinkForVid]); // get vid
 
-function draw() {
-    // if (!flick.pauseForFlick) vid.play();   // keep playing if not pausing
+        // hide default video behavior
+        vid.hide();
 
-    // I can't figure out how to get the video to pause while it is flickering...
-
-    if (!flick.flicker) { // if no flickering just draw the image and return
-        image(vid, 0, 0);
-        return;
+        vid.loop();
     }
 
-    if (flick.drawNow === 0) {              // if drawing black
-        if (flick.black > 0) {              // and still drawing black
-            background(0);                  // draw black
-            flick.black--;
-        } else {                            // but if reached the end
-            flick.drawNow = 1;              // switch to draw white
-            flick.black = flick.blackCount; // reset
+    p.draw = function () {
+        // if (!flick.pauseForFlick) vid.play();   // keep playing if not pausing
+
+        // I can't figure out how to get the video to pause while it is flickering...
+
+        if (!flick.flicker) { // if no flickering just draw the image and return
+            p.image(vid, 0, 0);
+            return;
         }
-    } else if (flick.drawNow === 1) {       // if drawing white
-        if (flick.white > 0) {              // and still drawing white
-            background(255);                // draw white
-            flick.white--;
-        } else {                            // but if reached the end
-            flick.drawNow = 2;              // switch to draw vid
-            flick.white = flick.whiteCount; // reset
+
+        if (flick.drawNow === 0) {              // if drawing black
+            if (flick.black > 0) {              // and still drawing black
+                p.background(0);                  // draw black
+                flick.black--;
+            } else {                            // but if reached the end
+                flick.drawNow = 1;              // switch to draw white
+                flick.black = flick.blackCount; // reset
+            }
+        } else if (flick.drawNow === 1) {       // if drawing white
+            if (flick.white > 0) {              // and still drawing white
+                p.background(255);                // draw white
+                flick.white--;
+            } else {                            // but if reached the end
+                flick.drawNow = 2;              // switch to draw vid
+                flick.white = flick.whiteCount; // reset
+            }
+        } else if (flick.drawNow === 2) {
+            if (flick.vid > 0) {
+                // if (flick.pauseForFlick) vid.play();
+                p.image(vid, 0, 0);
+                // if (flick.pauseForFlick) vid.pause();
+                flick.vid--;
+            } else {
+                flick.drawNow = 0;
+                flick.vid = flick.vidCount;
+            }
         }
-    } else if (flick.drawNow === 2) {
-        if (flick.vid > 0) {
-            // if (flick.pauseForFlick) vid.play();
-            image(vid, 0, 0);
-            // if (flick.pauseForFlick) vid.pause();
-            flick.vid--;
-        } else {
-            flick.drawNow = 0;
-            flick.vid = flick.vidCount;
-        }
+        // image(vid, 0, 0);
     }
-    // image(vid, 0, 0);
-}
+};
+
+new p5(sketch);
