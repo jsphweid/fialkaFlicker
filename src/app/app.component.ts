1bde3aaf2a0f262d2624d210bc72ae0a9f7242ee
Joseph Weidinger <jsphweid@gmail.com>
2018-12-05 09:49:41 -0800

    last of old

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,21 @@ export class AppComponent {
 		var my = this;
 		if (files.length === 1) {
 			var reader = new FileReader();
-			reader.onload = (function(theFile) {
+			reader.onload = (function(theFile) { // go here
+				let win: any = window;
+				my.flickerCycleService.p5file = new win.p5.File(theFile[0]);
 				return function(e) {
-					my.flickerCycleService.downloadLinkForVid = e.target.result;
-					new window.p5(my.flickerCycleService.sketch); 
+					// clear out the div
+					var node = document.getElementById('canvas');
+					while (node.hasChildNodes()) {
+						node.removeChild(node.firstChild);
+					}
+					// start new sketch / canvas with contents
+					// my.flickerCycleService.p5file.data = e.target.result; // e.target.result is blob?
+					my.flickerCycleService.p5file.data = e.target.result;
+					// debugger;
+					let win: any = window;
+        			new win.p5(my.flickerCycleService.sketch);
 				};
 			})(files);
 			reader.readAsDataURL(files[0]); // get first file of 1, i.e. [file]
